cfd3c4435674cf02bce9148ffb3bc59a830e961e
Joel Dapello <dapello@node091.ib.cluster>
2021-05-05 14:33:32 -0400

    added efficientnet-b0

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,32 @@
 from cornet import *
 
+from efficientnet_pytorch import EfficientNet
+
+def efficientnet_b0(pretrained=True):
+    assert pretrained == True
+    return EfficientNet.from_pretrained('efficientnet-b0')
+
 layer_maps = {
+    'efficientnet_b0' : {
+        'V1' : None,
+        'V2' : None,
+        'V4' : None,
+        'IT' : '_blocks.11',
+        'decoder' : '_blocks.15',
+    },
     'mobilenet_v2' : {
         'V1' : None,
         'V2' : None,
         'V4' : None,
         'IT' : 'features.14',
         'decoder' : 'classifier'
+    },
+    'mobilenet_v3_large' : {
+        'V1' : None,
+        'V2' : None,
+        'V4' : None,
+        'IT' : 'features.13',
+        'decoder' : 'avgpool'
     },
     'resnet18' : {
         'V1' : None,
@@ -26,7 +46,7 @@ layer_maps = {
         'V1' : 'V1',
         'V2' : 'V2',
         'V4' : 'V4',
-        'IT' : 'V4',
+        'IT' : 'IT',
         'decoder' : 'decoder.avgpool'
     }
 }
